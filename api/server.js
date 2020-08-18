81b6e1e69f4bdd080d84d5ac197559bdf798b85d
MaryamMosstoufi <maryam@zooxoos.com>
2020-08-18 18:53:06 -0400

    hash

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 
 const usersRouter = require('../users/usersRouter.js');
 
@@ -13,6 +14,17 @@ server.use(cors());
 
 server.use('/api/users', usersRouter);
 
+server.get('/hash', (req, res) => {
+  try {
+    const password = req.headers.password;
+    const rounds = process.env.HASH_ROUNDS || 8;
+    const hash = bcrypt.hashSync(password, rounds);
+    res.status(200).json({password,hash})
+  }
+  catch (error) {
+    res.status(500).json({error: error.message})
+  }
+})
 //Test
 server.get('/api', (req, res) => {
   res.status(200).json({ api: 'up' });
