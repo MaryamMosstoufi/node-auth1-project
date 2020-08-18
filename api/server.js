945cd62244c607d84e761f012f960fd5ce9d3e16
MaryamMosstoufi <maryam@zooxoos.com>
2020-08-18 10:35:26 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
+const helmet = require("helmet");
+const cors = require("cors");
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
+server.use(cors());
 
 server.get('/api', (req, res) => {
   res.status(200).json({ api: "up" });
