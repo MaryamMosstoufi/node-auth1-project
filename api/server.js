f6fbca565edb198131b437804d8f82891a0c386a
MaryamMosstoufi <maryam@zooxoos.com>
2020-08-18 18:16:49 -0400

    Initial Setup Redone
    This reverts commit 6ffa1a27b7a5a666ec7eaad6a9a837f46e3ade77.

--- api/server.js
+++ api/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const helmet = require("helmet");
-const cors = require("cors");
+const helmet = require('helmet');
+const cors = require('cors');
+
+const usersRouter = require('../users/usersRouter.js');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+
+server.use('/api/users', usersRouter);
+
+//Test
 server.get('/api', (req, res) => {
-  res.status(200).json({ api: "up" });
+  res.status(200).json({ api: 'up' });
 });
 
 module.exports = server;
