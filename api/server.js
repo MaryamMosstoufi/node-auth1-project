160149e9b6ba8e34bd7d39b23d20a57e895a8af1
MaryamMosstoufi <maryam@zooxoos.com>
2020-08-19 09:50:39 -0400

    auth

--- api/server.js
+++ api/server.js
@@ -1,18 +1,44 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
 const usersRouter = require('../users/usersRouter.js');
+const authRouter = require('../auth/authRouter.js');
+const dbConnection = require("../database/connection.js");
+const protected = require("../auth/protected-mw.js");
 
 const server = express();
+
+const sessionConfiguration = {
+    name: "monster",
+    secret: "keep it secret, keep it safe!",
+    cookie: {
+        maxAge: 1000 * 60 * 10, 
+        secure: process.env.COOKIE_SECURE || false, 
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: true, 
+    store: new KnexSessionStore({
+        knex: dbConnection,
+        tablename: "sessions",
+        sidfieldname: "sid",
+        createtable: true,
+        clearInterval: 1000 * 60 * 60, 
+    }),
+};
+
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfiguration));
 
-
-server.use('/api/users', usersRouter);
+server.use('/api/users', protected, usersRouter);
+server.use('/api/auth', authRouter);
 
 server.get('/hash', (req, res) => {
   try {
